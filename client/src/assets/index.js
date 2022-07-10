61afe2c4eaa1292020fdfb397d1810f8d3e24ca1
davidperjac <diperez@espol.edu.ec>
2022-07-10 02:10:03 -0500

    refactor installation and add env keys

---
+++ client/src/assets/index.js
@@ -0,0 +1,11 @@
+const assets = {
+	images: {
+		logo: require('./images/logo.png'),
+		icon: require('./images/favicon.png'),
+	},
+	colors: {
+		secondary: '#292929',
+	},
+};
+
+export default assets;
