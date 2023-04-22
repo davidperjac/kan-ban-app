03ace26a7c468f7eb2cce974f9789a6b2a19a00e
davidperjac <diperez@espol.edu.ec>
2023-04-22 00:45:08 -0500

    fix: change server host

--- client/src/api/axiosClient.js
+++ client/src/api/axiosClient.js
@@ -5,33 +5,33 @@ const baseURL = import.meta.env.VITE_SERVER;
 const getToken = () => localStorage.getItem('token');
 
 const axiosClient = axios.create({
-	baseURL: baseURL || 'http://127.0.0.1:3001/api/v1/',
-	paramsSerializer: (params) => queryString.stringify({ params }),
+  baseURL: baseURL || 'http://localhost:3001/api/v1/',
+  paramsSerializer: (params) => queryString.stringify({ params }),
 });
 
 axiosClient.interceptors.request.use(async (config) => {
-	return {
-		...config,
-		headers: {
-			'Content-Type': 'application/json',
-			authorization: `Bearer ${getToken()}`,
-		},
-	};
+  return {
+    ...config,
+    headers: {
+      'Content-Type': 'application/json',
+      authorization: `Bearer ${getToken()}`,
+    },
+  };
 });
 
 axiosClient.interceptors.response.use(
-	(response) => {
-		if (response && response.data) {
-			return response.data;
-		}
-		return response;
-	},
-	(err) => {
-		if (!err.response) {
-			return alert(err);
-		}
-		throw err.response;
-	}
+  (response) => {
+    if (response && response.data) {
+      return response.data;
+    }
+    return response;
+  },
+  (err) => {
+    if (!err.response) {
+      return alert(err);
+    }
+    throw err.response;
+  }
 );
 
 export default axiosClient;
